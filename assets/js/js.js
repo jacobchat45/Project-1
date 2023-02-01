88707a998334fb0b877ac81133e3b3f6c15d5905
Jacob Chatfield <jacob.chatfield45@outlook.com>
2023-02-01 20:54:08 +0000

    starting notes pushing progress

--- assets/js/js.js
+++ assets/js/js.js
@@ -1,1 +1,14 @@
-alert("linked");
+let notesInput = document.querySelector("#search-input")
+let notesButton = document.querySelector("#search-button");
+let inputResult;
+let returnedData;
+// this gets the value of what's submitted inside the form and then calls the getWeather function
+  notesButton.addEventListener("click", function(event){
+    event.preventDefault();
+   inputResult = notesInput.value;
+    console.log(inputResult); 
+    // add call to add notes function here
+  }
+    
+  )
+
